72658878a1b60363dc93c5ce6e6eef3f63113edb
MilesJohnson34 <milesivyjohnson@gmail.com>
2024-12-24 00:03:51 +0000

    yo

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import encoder 
 
 def main():
-    encoder.encode("DialogueTest","DialogueTestUpdated.mp4",.1,.1,10)
+    encoder.encode("DialogueTest","DTEST.mp4",.1,.1,10)
 
 if __name__== "__main__":
     main()
